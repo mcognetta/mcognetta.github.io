095647ddde61f57398488c25b53b737582551f36
Marco Cognetta <cognetta.marco@gmail.com>
2018-03-18 03:29:20 +0900

    changed theme a lot

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "testing testing",
+        "excerpt":"abcdefg","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2018/03/17/test/",
+        "teaser":null}]
