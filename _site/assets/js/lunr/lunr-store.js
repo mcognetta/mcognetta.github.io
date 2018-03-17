929d64ac4f4c57fdc860b45bc2ee33278bddba6e
Marco Cognetta <cognetta.marco@gmail.com>
2018-03-18 03:44:06 +0900

    base url

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "testing testing",
+        "title": "testing testing 2",
         "excerpt":"abcdefg","categories": [],
         "tags": [],
         "url": "http://localhost:4000/2018/03/17/test/",
